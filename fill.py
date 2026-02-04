1610ec53e71dceb22561f390d861d4095bea7c25
barbartek <bjanisz1@intercars.eu>
2026-02-04 22:09:09 +0100

    dodanie plików z folderu

--- fill.py
+++ fill.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route('/stats/day/<date>', methods=['GET'])
 def daily_stats(date):
- with open("data.txt", "r") as file:
+ with open("20250101.txt", "r") as file:
         first_line = file.readline().strip()
     return jsonify(first_line)
  
