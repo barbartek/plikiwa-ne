3812a5da12ed622389f8b8e53cf2fbd223d0dc38
barbartek <bjanisz1@intercars.eu>
2026-02-05 08:19:20 +0100

    nie jeden a wszystki

--- fill.py
+++ fill.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/stats/day/<date>', methods=['GET'])
 def daily_stats(date):
     with open("20250101.txt", "r") as file:
-        first_line = file.readline().strip()
+        all_line = file.readline().strip()
     return jsonify(first_line)
 
 app.run(debug=True)
