8a48fed2e81549996a3bd4eb6176ebcc700ae481
barbartek <bjanisz1@intercars.eu>
2026-02-05 09:20:35 +0100

    coco

--- fill.py
+++ fill.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/stats/day/<date>', methods=['GET'])
 def daily_stats(date):
-    with open("20250101.txt", "r") as file:
+    with open("./raw-data/20250501.txt", r, encoding="utf-8") as file:
         first_line = file.readline().strip()
     return jsonify(first_line)
 
